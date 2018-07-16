262b642bbe9e9bc5a2ff73697e7314ab063a7109
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-07-16 18:21:06 +0100

    week02-day01-node exercise final solution

--- week02-day01-node/index.js
+++ week02-day01-node/index.js
@@ -1,13 +1,35 @@
 const net = require('net');
 const server = net.createServer();
+let connections = [];
 
 server.on('connection', handleConnection);
-server.listen(9000, function() {
-  console.log('server listening to %j', server.address());
+server.listen(9000, () => {
+    console.log('server listening to %j', server.address());
 });
 
 function handleConnection(conn) {
-    const remoteAddress = `${conn.remoteAddress}:${conn.remotePort}`;
-    console.log(`new client connection from ${remoteAddress}`);
-  }
-  
+    const remoteAddressAndPort = `${conn.remoteAddress}:${conn.remotePort}`;
+    connections.push(conn);
+    console.log(`new client connection from ${remoteAddressAndPort}; connections: ${connections.length}`);
+
+    conn.on('data', (buffer) => {
+        const textFileName = 'sample.txt';
+        const data = buffer.toString();
+        const now = new Date().toTimeString().substr(0, 8);
+        const message = `${now} Got "${data.trim()}". Let me uppercase: "${data.trim().toUpperCase()}"\n`;
+        console.log(`Received from ${remoteAddressAndPort} :`, data);
+        // conn.write(message);
+        connections
+            .filter((connection) => conn !== connection)
+            .map((connection) => {
+                connection.write(message);
+            });
+    });
+    conn.on('error', (error) => {
+        console.log('An error occurred:', error.message);
+    })
+    conn.on('close', () => {
+        console.log(`Closed: ${remoteAddressAndPort}`);
+        connections = connections.filter((connection) => conn !== connection);
+    });
+}
