190b9b4263a9bdaec8b2989b9a3c939c51ea5eaa
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-07-17 16:45:14 +0100

    week02-day02-express implement quotes button version 2

--- week02-day02-express/public/main.js
+++ week02-day02-express/public/main.js
@@ -1,31 +1,25 @@
 console.log('in main.js');
 
-function appendQuotes(quotes) {
+function appendQuote(quote) {
     const quotesListElement = document.getElementById('quotes-list');
+    const quoteHtml = `
+        <li>
+            <strong>${quote.name}</strong>: ${quote.text}
+        </li>
+    `;
 
-    quotes.forEach((quote) => {
-        const quoteHtml = `
-            <li>
-                <strong>${quote.name}</strong>: ${quote.text}
-            </li>
-        `;
-
-        quotesListElement.innerHTML += quoteHtml;
-    });
+    quotesListElement.innerHTML += quoteHtml;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const quotesButtonElement = document.getElementById('quotes');
 
     quotesButtonElement.addEventListener('click', () => {
-        const quotesUrl = '/quotes';
-
-        fetch(quotesUrl).then(
+        fetch('/quotes').then(
             (response) => response.json()
         ).then((quotes) => {
             console.log('quotes:', quotes);
-            appendQuotes(quotes);
+            quotes.map(appendQuote);
         });
     });
 });
-
