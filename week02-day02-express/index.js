a8978e8636288ff036b4d33fef4b3cc542e0f509
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-07-18 12:38:22 +0100

    week02-day02-express Exercise 6: add new quote

--- week02-day02-express/index.js
+++ week02-day02-express/index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3300;
@@ -23,8 +24,12 @@ app.use((request, response, next) => {
 
 app.use(express.static('public'));
 
+
 app.get('/quotes', (request, response) => {
     response.send(quotes);
+});
+app.get('/quotes/:name', function (request, response) {
+    response.send(request.params.name);
 });
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
@@ -34,6 +39,35 @@ function diyLogger(request, response, next) {
     const startTime = startDate.getTime();
     const { url, method } = request;
     const { statusCode } = response;
+
+    app.get('/quotes/:name', (request, response) => {
+        const { name } = request.params;
+        const matchedQuote = quotes.find((quote) => {
+            return name === quote.name;
+        });
+    
+        if (!matchedQuote) {
+            response.status(404).json('That person isn\'t quote-worthy.');
+        } else {
+            response.json(matchedQuote.text);
+        }
+    });
+
+
+
+    app.post('/quotes', bodyParser.json(), function(request, response) {
+        const newQuote = request.body;
+        // Add your new quote to the quotes array
+        // Then send back a 201 status
+        // And also send the new quote as JSON in the response
+      });
+
+      document.getElementById("create-quote").addEventListener("submit", myFunction);
+
+      function myFunction() {
+          alert("The form was submitted");
+      }
+
 
     response.on('finish', () => {
         const finishDate = new Date();
--- week02-day02-express/public/main.js
+++ week02-day02-express/public/main.js
@@ -12,14 +12,61 @@ function appendQuote(quote) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const quotesButtonElement = document.getElementById('quotes');
+    const getQuotesButtonElement = document.getElementById('quotes');
+    const createQuoteFormElement = document.getElementById('create-quote');
+    const quotesListElement = document.getElementById('quotes-list');
 
-    quotesButtonElement.addEventListener('click', () => {
+    getQuotesButtonElement.addEventListener('click', () => {
         fetch('/quotes').then(
             (response) => response.json()
         ).then((quotes) => {
             console.log('quotes:', quotes);
             quotes.map(appendQuote);
         });
+    });
+
+    quotesListElement.addEventListener('click', (event) => {
+        const { clickedElement } = event;
+
+        if (clickedElement.tagName === 'BUTTON') {
+            const name = clickedElement.getAttribute('name');
+
+            fetch(`/quotes/${name}`, {
+                method: 'DELETE',
+            }).then(() => {
+                const liElement = clickedElement.parentNode;
+                liElement.parentNode.removeChild(liElement);
+            });
+        }
+    });
+    
+    createQuoteFormElement.addEventListener('submit', (event) => {
+        console.log('create quote: event:', event);
+        const name = event.target.name.value;
+        const text = event.target.text.value;
+        const newQuote = { name, text };
+        event.preventDefault();
+        console.log('newQuote:', newQuote);
+
+        fetch(
+            '/quotes',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+                body: JSON.stringify(newQuote),
+            }
+        ).then(
+            (response) => response.json()
+        ).then(
+            (quote) => {
+                appendQuote(quote);
+                event.target.name.value = '';
+                event.target.text.value = '';
+            }
+        ).catch(
+            error => console.error(`Fetch Error =\n`, error)
+        );
     });
 });
