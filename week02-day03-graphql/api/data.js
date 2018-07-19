f5014bf8008be6c1b4fe941345a0d233b05204aa
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-07-19 13:46:07 +0100

    week02-day03-graphql Exercise 3 add helpers

--- week02-day03-graphql/api/data.js
+++ week02-day03-graphql/api/data.js
@@ -73,4 +73,4 @@ const data = {
     ]
   };
   
-  module.exports =  data ;
+  module.exports =  data;
--- week02-day03-graphql/api/resolvers.js
+++ week02-day03-graphql/api/resolvers.js
@@ -1,33 +1,21 @@
-const { data } = require('./data');
+const { PersonModel, MovieModel } = require('./models');
+
 const resolvers = {
     Query: {
-        people() {
-            // TODO
-        },
-        person(root, { id }) {
-            // TODO
-        },
-        movies() {
-            return data.movies;
-        },
-        movie(root, { id }) {
-            return data.movies.find((movie) => movie.id === parseInt(id));
-        }
+        people: () => PersonModel.getMany(),
+        person: (root, { id }) => PersonModel.getOne(id),
+        movies: (root, { ids }) => MovieModel.getMany(ids),
+        movie: (root, { id }) => MovieModel.getOne(id)
+    },
+    Person: {
+        filmography: (person) => PersonModel.getFilmography(person)
     },
     Movie: {
-        stars(movie) {
-            return data.people.filter((person) => (
-                person.filmography.find((credit) => (
-                    credit === movie.id && person.id !== movie.director
-                ))
-            ));
-        },
-        director(movie) {
-            if (!movie.director) {
-                return null;
-            };
-            return data.people.find((person) => person.id === movie.director);
-        }
+        stars: (movie) => MovieModel.getStars(movie),
+        director: (movie) => MovieModel.getDirector(movie)
+    },
+    Mutation: {
+        addPerson: (root, { person }) => PersonModel.addOne(person)
     }
 };
 
