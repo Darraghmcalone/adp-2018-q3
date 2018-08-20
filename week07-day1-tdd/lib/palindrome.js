12369fcb07aef6a1712209702c72f61ca832145c
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-08-20 17:20:52 +0100

    palindrome tdd

---
+++ week07-day1-tdd/lib/palindrome.js
@@ -0,0 +1,25 @@
+/**
+ * @description Check if any word is a palindrome
+ * @returns {Boolean} the specified element of the Fibonacci sequence.
+ */
+
+function palindrome(input) {
+    if (input.length === 1) {
+        return true;
+    }
+
+    if (typeof input !== "string") {
+        return false;
+    }
+
+    const lowerCase = input.toLowerCase();
+    const sanitised = lowerCase.replace(/ /g, '');
+    const reversed = sanitised.split('').reverse().join('');
+    if (reversed === sanitised) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+module.exports = palindrome; 
